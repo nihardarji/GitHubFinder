7a2adb9678f1a91e6faeb9ff438404eddbec2bc3
Nihar Darji <nihardarji@Nihars-MacBook-Pro.local>
2020-05-27 00:56:10 -0500

    Initial commmit

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+
+export const About = () => {
+    return (
+        <Fragment>
+            <h1> About this Application</h1>
+            <p> App to search GitHub users</p>
+        </Fragment>
+    )
+}
